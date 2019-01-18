8f70c11461f27b5036ff5edee718f0bbe97c8d13
Tyson <tyelf22@hotmail.com>
2019-01-17 22:46:44 -0800

    jquery added to change html

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function calculate() {
-    let amount = $('#amount');
-    let amountString = amount.val();
-    let amountNumber = parseInt(amountString, 10);
-    let startAmount = 500;
+    var amount = $('#amount');
+    var amountString = amount.val();
+    var amountNumber = parseInt(amountString, 10);
+    var startAmount = 500;
     
     console.log(amountNumber);
 
-    if(amountNumber >= 0.1) {
-        console.log(`you deposited ${amountNumber},you now have ${amountNumber + startAmount}`);
+    if(amountNumber != false) {
+        var totalNumber = amountNumber + startAmount;
+        $('#total').html('Your new total is: $' + totalNumber);
+    } else{
+        $('#total').html('Insufficient Funds');
     }
 }
